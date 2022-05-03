4794cea8c617e14a0a49008364d4c7f67ec84b61
Shamil9310 <Itachi9310@yandex.ru>
2022-05-03 14:18:57 +0300

    Выполнено 6-е задание

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -17,4 +17,10 @@ Outbound Interface    FastEthernet0/0
 проверять результат.
 """
 
-ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+ospf_route = "  10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+result = '''Prefix                10.0.24.0/24
+AD/Metric             110/41
+Next-Hop              10.0.13.3
+Last update           3d18h
+Outbound Interface    FastEthernet0/0 '''
+print(result)
